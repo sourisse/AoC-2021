671c8b23ad6bd57b68aec493cc71d5904229a274
sourisse <mattsourisseau@gmail.com>
2021-12-02 16:45:29 -0700

    Add files via upload
    Days 1 & 2

---
+++ aoc_1/aoc_1.sh
@@ -0,0 +1,33 @@
+# Part 1 of AOC-1
+
+input=aoc_1_in.txt
+
+function offsetter (){
+	# input:  aoc input filename
+	# output: all but the first $2 lines of the input file
+	# assumptions: at least 10 lines in the input file.
+
+	tail -n $(( $(wc -l "$input" | egrep -o '\d\d+' ) - $1 )) "$input"
+}
+
+# Unfortunately the paste command later complains of long input if I try to redirect,
+# so to move on I just saved the output into two files.
+
+offsetter 1 > aoc_1-1.txt
+offsetter 3 > aoc_1-2.txt
+
+# Soution to part 1
+
+while read line; do 
+
+	echo $(( $line ))
+
+done <<< "$(paste -d \- "$input" aoc_1-1.txt | egrep -o '\d+-\d+')" | egrep -o '-' | wc -l
+
+# Solution to part 2
+
+while read line; do
+
+	echo $(( $line ))
+
+done <<< "$(paste -d \- "$input" aoc_1-2.txt | egrep -o '\d+-\d+')" | egrep -o '-' | wc -l
---
+++ aoc_2/aoc_2.sh
@@ -0,0 +1,48 @@
+
+# Part 1 of AOC-1
+
+dep=0; pos=0
+
+while read line; do
+
+	x="$(echo "$line" | egrep -o '\d+')"
+
+	if [[ "$(echo $line | egrep -o 'forward' )" ]]; then
+		pos=$(( $pos + $x ))
+	
+	elif [[ "$(echo $line | egrep -o 'up' )" ]]; then
+		dep=$(( $dep - $x ))
+	
+	elif [[ "$(echo $line | egrep -o 'down' )" ]]; then
+		dep=$(( $dep + $x ))
+	
+	fi
+
+done < aoc_2_in.txt
+
+echo "$(( $pos * $dep ))"
+
+
+# Part 2 of AOC-2
+
+aim=0; dep=0; pos=0
+
+while read line; do
+
+	x="$(echo "$line" | egrep -o '\d+')"
+
+	if [[ "$(echo $line | egrep -o 'forward' )" ]]; then
+		pos=$(( $pos + $x ))
+		dep=$(( $dep + $aim * $x ))
+	
+	elif [[ "$(echo $line | egrep -o 'up' )" ]]; then
+		aim=$(( $aim - $x ))
+	
+	elif [[ "$(echo $line | egrep -o 'down' )" ]]; then
+		aim=$(( $aim + $x ))
+	
+	fi
+
+done < aoc_2_in.txt
+
+echo "$(( $pos * $dep ))"
